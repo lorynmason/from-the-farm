8672f397951ee34d3c09f57bb75e14f8edf67940
Mike Duke <aithon@gmail.com>
2019-02-13 11:45:45 -0700

    Map markers working

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import Buy from '../Buy/Buy';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      coordinates: [[39.7392, -104.9903], [40.0150, -105.2705], [39.6133, -105.0166], [39.7294, -104.8319]]
+    }
+  }
 
   render() {
     return (
       <div className="App">
-        <Buy />
+        <Buy coordinates={this.state.coordinates} />
       </div>
     );
   }
---
+++ src/Components/Map/Map.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import accessToken from '../../mapbox-accessToken.js';
+import L from 'leaflet';
+
+export class Map extends Component {
+
+  componentDidMount() {
+    const map = L.map('map').setView([39.750614, -104.996775], 9);
+    const { coordinates } = this.props;
+
+    coordinates.forEach((location) => {
+      console.log(location)
+      const marker = L.marker(location).addTo(map);
+      marker.bindPopup(`<h4>${location}</h4>`)
+    });
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken
+    }).addTo(map);
+  }
+
+  render() {
+    const dimensions = {
+      height: '400px',
+      width: '400px'
+    }
+
+    return (
+      <article id="map" style={dimensions}></article>
+    )
+  }
+}
+
+export default Map;
