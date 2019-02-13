4929718eb4b6acf39f7caa71d44dce73034a61de
Loryn Mason <masonloryn@gmail.com>
2019-02-13 13:14:24 -0700

    Style Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,9 +3,9 @@ import React from 'react';
 export const Header = () => {
   return (
     <header>
-      <h1>from the farm</h1>
+      <h1>from<span>the</span>farm</h1>
       <img id="logo" src="https://www.shareicon.net/data/2016/06/18/596437_pig_512x512.png" alt="from the farm cute pig logo"/>
-      {/* <img id="logo" src="../../../public/assets/596437_pig_512x512.png" alt="from the farm cute pig logo"/> */}
+      {/* <img id="logo" src="/596437_pig_512x512.png" alt="from the farm cute pig logo"/> */}
     </header>
   )
 }
