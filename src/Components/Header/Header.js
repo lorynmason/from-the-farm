833831fdd291daf09ad0eda7841b747410748793
Loryn Mason <masonloryn@gmail.com>
2019-02-13 10:55:10 -0700

    Add basic header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>from the farm</h1>
+      <img id="logo" src="https://www.shareicon.net/data/2016/06/18/596437_pig_512x512.png" alt="from the farm cute pig logo"/>
+      {/* <img id="logo" src="../../../public/assets/596437_pig_512x512.png" alt="from the farm cute pig logo"/> */}
+    </header>
+  )
+}
