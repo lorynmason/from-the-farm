307997de08eadfd116e2613c77b4f5accec8b17a
Loryn Mason <masonloryn@gmail.com>
2019-02-13 15:14:08 -0700

    Edit header for router link

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,9 +5,11 @@ import { Link } from 'react-router-dom';
 export const Header = () => {
   return (
     <header>
-      <h1>from<span>the</span>farm</h1>
-      <img id="logo" src="https://www.shareicon.net/data/2016/06/18/596437_pig_512x512.png" alt="from the farm cute pig logo"/>
       <Link to="/profile"><i className="fas fa-bacon"></i></Link>
+      <div className="header-logo">
+        <h1>from<span>the</span>farm</h1>
+        <img id="logo" src="https://www.shareicon.net/data/2016/06/18/596437_pig_512x512.png" alt="from the farm cute pig logo"/>
+      </div>
     </header>
   )
 }
