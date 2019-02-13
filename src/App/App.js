d682a15a111fc815d620466b1e7cdf31cea18663
Loryn Mason <masonloryn@gmail.com>
2019-02-13 10:20:12 -0700

    Initial setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Buy } from './Components/Buy';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
