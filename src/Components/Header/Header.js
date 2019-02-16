d58e2a1069975747c73c5d80a356343501c3636b
Loryn Mason <masonloryn@gmail.com>
2019-02-16 08:26:08 -0700

    Add fade-in menu to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,9 +7,18 @@ export const Header = () => {
   return (
     <header>
       {/* <Link to="/profile"><i className="fas fa-bacon"></i></Link> */}
-      <div className="header-logo">
-        <h1>from<span>the</span>farm</h1>
-        <img id="logo" src={image} alt="from the farm cute pig logo"/>
+      <div className="container">
+        <div className="header-logo">
+          <h1>from<span>the</span>farm</h1>
+          <img id="logo" src={image} alt="from the farm cute pig logo"/>
+        </div>
+        <div className="menu">
+          <i className="fas fa-carrot"></i>
+          <i className="fas fa-user-circle"></i>
+          <i className="fas fa-plus-circle"></i>
+          <i className="fas fa-leaf"></i>
+          <i className="fas fa-sign-out-alt"></i>
+        </div>
       </div>
     </header>
   )
