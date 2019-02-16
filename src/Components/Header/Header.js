d39a93ea9b1f488783c85a00cfbd170b13575322
Loryn Mason <masonloryn@gmail.com>
2019-02-16 07:35:24 -0700

    Completly restyle header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,7 @@ import image from '../../styles/images/pig-logo.png'
 export const Header = () => {
   return (
     <header>
-      <Link to="/profile"><i className="fas fa-bacon"></i></Link>
+      {/* <Link to="/profile"><i className="fas fa-bacon"></i></Link> */}
       <div className="header-logo">
         <h1>from<span>the</span>farm</h1>
         <img id="logo" src={image} alt="from the farm cute pig logo"/>
