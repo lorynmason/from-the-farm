b19b4a85ff7f721dd27c2b4bf2132a3d51243099
Mike Duke <aithon@gmail.com>
2019-02-17 13:51:58 -0700

    Additional snapshot tests written for Profile and ProductList

--- src/Components/Map/Map.test.js
+++ src/Components/Map/Map.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Map } from './Map';
 import { shallow, mount } from 'enzyme';
+import { createBrowserHistory } from 'history';
+
+let mockHistory = jest.mock('history');
 
 describe('Map', () => {
   it.skip('should match snapshot', () => {
-    let wrapper = mount(<Map coordinates="[40, -140]" />)
+    let wrapper = shallow(<Map coordinates="[40, -140]" history={{listen: jest.fn()}} />)
+    console.log(wrapper.instance().props)
+    wrapper.setProps({history: {listen: jest.fn()}});
     expect(wrapper).toMatchSnapshot()
   });
-})
+});
 
 // known issues with testing Leaflet with React exist. Will continue to research solutions
