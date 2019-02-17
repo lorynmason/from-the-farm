1f2499244eafd6afcbbcd0ce66e1f95016ed555b
Mike Duke <aithon@gmail.com>
2019-02-16 18:52:01 -0700

    Additional changes on last commit

--- src/Components/Buy/Buy.js
+++ src/Components/Buy/Buy.js
@@ -4,7 +4,6 @@ import ProductList from '../ProductList/ProductList';
 import Search from '../Search/Search'
 
 export const Buy = (props) => {
-  const { vendors, products } = props.appState;
   return (
     <section className="buy">
       <Search search={props.search}/>
