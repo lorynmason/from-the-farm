db5b3aaf680ab2d2f617932464fcacc1f9fc8768
Mike Duke <aithon@gmail.com>
2019-02-17 11:02:03 -0700

    Testing for Search component passing with current functionality

--- src/Components/Search/Search.test.js
+++ src/Components/Search/Search.test.js
@@ -1,10 +1,78 @@
 import React from 'react';
-import Search from './Search';
-import { shallow } from 'enzyme';
+import { Search, mapStateToProps, mapDispatchToProps } from './Search';
+import { shallow, mount } from 'enzyme';
+import { addProducts } from '../../actions';
 
 describe('Search', () => {
+  let wrapper = shallow(<Search products={[]} search={jest.fn()} />);
+
   it('should match snapshot', () => {
-    let wrapper = shallow(<Search products={[]} />)
-    expect(wrapper).toMatchSnapshot()
+    expect(wrapper).toMatchSnapshot();
   });
-})
+
+  describe('handleChange', () => {
+    it('should update state when changes are made to form elements', () => {
+      expect(wrapper.state()).toEqual({
+        productId: '',
+        location: '',
+        range: ''
+      });
+
+      wrapper.find('.search').simulate('change', {'target': {name: 'productId', value: '1'}});
+      wrapper.find('.search').simulate('change', {'target': {name: 'location', value: '80219'}});
+      wrapper.find('.search').simulate('change', {'target': {name: 'range', value: '50'}});
+
+      expect(wrapper.state()).toEqual({
+        productId: '1',
+        location: '80219',
+        range: '50'
+      });
+    });
+  });
+
+  describe('sendSearch', () => {
+    it('should call search with the correct params on submit', () => {
+      const wrapper = shallow(<Search products={[]} />);
+      wrapper.setProps({search: jest.fn()});
+
+      const { search } = wrapper.instance().props
+      wrapper.find('.search').simulate('submit', {preventDefault: jest.fn()});
+
+      const expected = {
+        productId: '',
+        location: '',
+        range: ''
+      }
+
+      expect(search).toHaveBeenCalledWith(expected);
+    });
+  });
+
+  describe('mapStateToProps', () => {
+    it('should return an array of products', () => {
+      const mockGlobalState = {
+        vendors: [{id: 1, name: 'Farm Team'}],
+        products: [{name: 'carrots', quantity: 'all'}],
+        user: {name: 'Mike'}
+      }
+
+      const expected = [{name: 'carrots', quantity: 'all'}];
+
+      const mappedProps = mapStateToProps(mockGlobalState);
+      expect(mappedProps.products).toEqual(expected);
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    const mockDispatch = jest.fn();
+    it('should dispatch addProducts with the correct params when addProductsToStore is called', () => {
+      const mockProducts = [{name: 'carrots', quantity: 'all'}];
+      const expected = addProducts(mockProducts);
+
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.addProductsToStore(mockProducts);
+
+      expect(mockDispatch).toHaveBeenCalledWith(expected);
+    });
+  });
+});
