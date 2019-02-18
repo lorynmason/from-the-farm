76fbbdca983695e931dcb030ea5eb4bf4a6d7f6a
Loryn Mason <masonloryn@gmail.com>
2019-02-18 12:13:25 -0700

    Add login func

---
+++ src/thunks/fetchUser.js
@@ -0,0 +1,22 @@
+import { isLoading, hasErrored, addUser } from '../actions';
+import { cleanUser } from '../helpers/cleaner'
+
+export const fetchUser = (url, token) => {
+  return async (dispatch) => {
+    try {
+      dispatch(isLoading(true));
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      dispatch(isLoading(false))
+      const results = await response.json();
+      const user = cleanUser(results.data, token)
+      console.log(user)
+      dispatch(addUser(user)) 
+    } catch (error) {
+      console.log(error)
+      dispatch(hasErrored(error.message));
+    }
+  }
+}
