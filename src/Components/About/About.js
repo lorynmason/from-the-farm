83140ed3e7a624c215cca731afa8d5eb2eb0b403
Loryn Mason <masonloryn@gmail.com>
2019-02-17 19:10:55 -0700

    Add about page

---
+++ src/Components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+  <main>
+    <div className="about">
+      <p>From the Farm is a community based effort to break the traditional food supply chain and connect Farmers and Producers directly to Restaurants and Markets.
+      </p>
+    </div>
+  </main>
+  )
+}
