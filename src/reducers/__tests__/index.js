3413faf61a0601a20106ab9f0005c4d1b9a34ab6
Mike Duke <aithon@gmail.com>
2019-02-19 14:52:48 -0700

    Removed rootReducer test and replaced with snapshot

--- src/reducers/__tests__/index.js
+++ src/reducers/__tests__/index.js
@@ -1,67 +1,7 @@
 import { rootReducer } from '../index';
-import { createStore } from 'redux';
-import { userReducer } from '../userReducer';
-import { isLoadingReducer } from '../isLoadingReducer';
-import { hasErroredReducer } from '../hasErroredReducer';
-import { vendorReducer } from '../vendorReducer';
-import { productsReducer } from '../productsReducer';
-import { messageReducer } from '../messageReducer';
-import { itemsReducer } from '../itemsReducer';
-import * as Search from '../searchResultsReducer';
 
 describe('rootReducer', () => {
-  let store = createStore(rootReducer);
-  it('should add default state of vendors to the store when vendorReducer handles an action', () => {
-    const expected = vendorReducer([], {type: ''});
-    const results = store.getState().vendors;
-    expect(results).toEqual(expected);
-  });
-  
-  it('should add default state of products to the store when productsReducer handles an action', () => {
-    const expected = productsReducer([], {type: ''});
-    const results = store.getState().products;
-    expect(results).toEqual(expected);
-  });
-
-  it('should add default state of user to the store when userReducer handles an action', () => {
-    const expected = userReducer({}, {type: ''});
-    const results = store.getState().user;
-    expect(results).toEqual(expected);
-  });
-
-  it('should add default state of error to the store when hasErroredReducer handles an action', () => {
-    const expected = hasErroredReducer('', {type: ''});
-    const results = store.getState().hasErrored;
-    expect(results).toEqual(expected);
-  });
-
-  it('should add default state of message to the store when messageReducer handles an action', () => {
-    const expected = messageReducer('', {type: ''});
-    const results = store.getState().message;
-    expect(results).toEqual(expected);
-  });
-
-  it('should add default state of isLoading to the store when isLoadingReducer handles an action', () => {
-    const expected = isLoadingReducer(false, {type: ''});
-    const results = store.getState().isLoading;
-    expect(results).toEqual(expected);
-  });
-
-  it('should add default state of items to the store when itemsReducer handles an action', () => {
-    const expected = itemsReducer([], {type: ''});
-    const results = store.getState().items;
-    expect(results).toEqual(expected);
-  });
-
-  it('should add default state of vendorSearchResults to the store when vendorSearchReducer handles an action', () => {
-    const expected = Search.vendorSearchReducer([], {type: ''});
-    const results = store.getState().vendorSearchResults;
-    expect(results).toEqual(expected);
-  });
-
-  it('should add default state of productSearchResults to the store when productSearchReducer handles an action', () => {
-    const expected = Search.productSearchReducer([], {type: ''});
-    const results = store.getState().productSearchResults;
-    expect(results).toEqual(expected);
+  it('should match snapshot', () => {
+    expect(rootReducer).toMatchSnapshot();
   });
 });
