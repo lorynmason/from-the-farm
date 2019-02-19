686ef82dd1d037f076878e510c0c0f8914cea404
Mike Duke <aithon@gmail.com>
2019-02-18 19:56:29 -0700

    Fix tests with merge conflicts

--- src/helpers/mockVendors.js
+++ src/helpers/mockVendors.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const mockVendors = [
   {
     "id": "5",
@@ -38,43 +37,5 @@ export const mockVendors = [
                 }
             }]
           }
-=======
-const mockVendors = [{"id": "5",
-        "type": "user",
-        "attributes": {
-            "name": "Basil Connection",
-            "account_type": "vendor",
-            "address": "4950 Beach Court",
-            "city": "denver",
-            "state": "CO",
-            "zip": 80221,
-            "email": "helter_skelter@example.com",
-            "phone": 2313414141,
-            "lat": 39.7861784,
-            "long": -105.0178452,
-            "bio": "The WORLDS Freshest Basil",
-            "img_url": null,
-            "products": [
-                {
-                    "Berries": {
-                        "id": 3,
-                        "user_id": 5,
-                        "item_id": 1,
-                        "price": 1400,
-                        "unit": "lb",
-                        "description": "lb of berries"
-                    }
-                },
-                {
-                    "Potatoes": {
-                        "id": 17,
-                        "user_id": 5,
-                        "item_id": 4,
-                        "price": 170,
-                        "unit": "lb",
-                        "description": "a lb of potatoes"
-                    }
-                }]
-              }
->>>>>>> Cleaner testing
-}]
+  }
+]
