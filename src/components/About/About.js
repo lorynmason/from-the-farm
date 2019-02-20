27a8031c2abb395fe772f4d3a9156fedac9239e5
Mike Duke <aithon@gmail.com>
2019-02-20 16:40:31 -0700

    Used built-in VSCode linter and made relevant changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 export const About = () => {
   return (
-  <main>
-    <div className="about">
-      <p>From the Farm is a community based effort to break the traditional food supply chain and connect Farmers and Producers directly to Restaurants and Markets.
-      </p>
-    </div>
-  </main>
-  )
-}
+    <main>
+      <div className="about">
+        <p>
+          From the Farm is a community based effort to break the traditional food supply chain and connect Farmers and Producers directly to Restaurants and Markets.
+        </p>
+      </div>
+    </main>
+  );
+};
