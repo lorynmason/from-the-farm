08d6dacdfbe9230e5b231be69a4dcf8acf53824e
Mike Duke <aithon@gmail.com>
2019-02-19 21:17:12 -0700

    SearchVendors thunk tested and passing

--- src/thunks/searchVendors.js
+++ src/thunks/searchVendors.js
@@ -1,5 +1,5 @@
 import { isLoading, hasErrored, vendorSearchResults, productSearchResults, addMessage } from '../actions';
-import { cleanVendors, cleanProducts } from '../helpers/cleaner'
+import { filterSearchResults } from '../helpers/cleaner'
 
 export const searchVendors = (url, productId) => {
   return async (dispatch) => {
@@ -11,31 +11,13 @@ export const searchVendors = (url, productId) => {
       }
       dispatch(isLoading(false))
       const results = await response.json();
-      console.log(results)
       if(!results.data.length) {
         dispatch(addMessage('No Search Results'))
       }
-      const ids = results.data.reduce((arr, result) => {
-        if(!arr.includes(result.id)) {
-          arr.push(result.id)
-        }
-        return arr
-      },[])
-      const matches = ids.map(id => results.data.find(result => result.id === id))
-      let vendors = cleanVendors(matches);
-      let products = cleanProducts(matches);
-      vendors = vendors.map(vendor => vendor.id)
-      if(productId) {
-        products = products.filter(product => {
-          return product.item_id === parseInt(productId)
-        }).map(product => product.id)
-      } else {
-        products = products.map(product => product.id)
-      }
-      dispatch(vendorSearchResults(vendors))
-      dispatch(productSearchResults(products))
+      const filteredResults = filterSearchResults(results.data, productId);
+      dispatch(vendorSearchResults(filteredResults.vendors));
+      dispatch(productSearchResults(filteredResults.products));
     } catch (error) {
-      console.log(error)
       dispatch(hasErrored(error.message));
     }
   }
