60244c2195041d6a71329ab247d35619c7d44f0f
Loryn Mason <masonloryn@gmail.com>
2019-02-19 17:56:14 -0700

    Fix Map bug

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,58 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react'
 import accessToken from '../../mapbox-accessToken.js';
-import L from 'leaflet';
 import { connect } from 'react-redux';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
 
-export class Map extends Component {
-  componentDidUpdate() {
-    this.createMap();
+const Mapp = ({ vendors, vendorSearchResults }) => {
+  const position = [39.750614, -104.996775]
+  let vendorsToShow = vendors
+  if(vendorSearchResults.length) {
+    vendorsToShow = vendors.filter( vendor => {
+      return vendorSearchResults.includes(vendor.id) 
+    })
   }
 
-  createMap = () => {
-    const container = L.DomUtil.get('map');
+  const markers = vendorsToShow.map((vendor) => (
+    <Marker position={[vendor.lat, vendor.long]}>
+      <Popup>
+        <h3>${vendor.name}</h3>
+        <p>${vendor.address}</p>
+        <p>${vendor.phone}</p>
+        <p>${vendor.email}</p>
+      </Popup>
+    </Marker>
+  ));
 
-    if (container !== null) {
-      container._leaflet_id = null;
-    }
-
-    const map = L.map('map').setView([39.750614, -104.996775], 11);
-    const { vendors, vendorSearchResults } = this.props;
-    let vendorsToShow = vendors
-    if(vendorSearchResults.length) {
-      vendorsToShow = vendors.filter( vendor => {
-        return vendorSearchResults.includes(vendor.id) 
-      })
-    }
-    vendorsToShow.forEach((vendor) => {
-      const loc = [vendor.lat, vendor.long]
-      const marker = L.marker(loc).addTo(map);
-      const popup = `<h3>${vendor.name}</h3>
-      <p>${vendor.address}</p>
-      <p>${vendor.phone}</p>
-      <p>${vendor.email}</p>`
-      marker.bindPopup(popup)
-    });
-
-
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox.dark',
-      accessToken
-    }).addTo(map);
-  }
-
-  render() {
-    // this.props.history.listen((location, action) => {
-    //   if (location.pathname === '/buy') {
-    //     this.createMap();
-    //   }
-    // });
-
-    return (
-      <section id="map"></section>
-    );
-  }
+  return (
+    <Map center={position} zoom={11}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+      />
+      {markers}
+    </Map>
+  )
 }
 
 const mapStateToProps = (state) => ({
@@ -60,4 +40,4 @@ const mapStateToProps = (state) => ({
   vendorSearchResults: state.vendorSearchResults
 });
 
-export default connect(mapStateToProps)(Map);
+export default connect(mapStateToProps)(Mapp);
