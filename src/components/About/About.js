6f816220c859e017841f85690f9c13cc5317d2b9
Loryn Mason <masonloryn@gmail.com>
2019-03-07 15:58:25 -0700

    Edit about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,13 @@ export const About = () => {
     <main>
       <div className="about">
         <p>
-          From the Farm is a community based effort to break the traditional food supply chain and connect Farmers and Producers directly to Restaurants and Markets.
+          from the farm is a community based effort to break the traditional food supply chain and connect Farmers and Producers directly to Restaurants and Markets.
+        </p>
+        <p>
+          Please note that from the farm is still in the development phase, which means all data currently on the site is a placeholder, and doesn't represent real producers or products. 
+        </p>
+        <p>
+          If you'd like to contribute code or find out more information about from the farm please visit:<a>https://github.com/lorynmason/from-the-farm</a>
         </p>
       </div>
     </main>
